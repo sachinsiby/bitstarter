37a23f3ebd32120c73895e7ce11706ba89074c0b
Sachin Siby <sachinsiby@gmail.com>
2013-07-09 18:46:08 -0400

    part 1

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello friends!');
+ 
+ fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  response.send(data);
+  });
 });
 
 var port = process.env.PORT || 5000;
